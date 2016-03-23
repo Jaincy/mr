41e8d513f0d68363c626f2243951ac5692a8c76a
Jaincy <609447971@qq.com>
2016-03-23 17:43:51 +0800

    持续完善

--- src/com/yp114/omc/nsp/AbiQueryNum.java
+++ src/com/yp114/omc/nsp/AbiQueryNum.java
@@ -65,33 +65,33 @@ public class  AbiQueryNum extends Configured implements
                 throws IOException, InterruptedException {
             // TODO Auto-generated method stub
             String line = value.toString().trim().replaceAll("\t", "");
-            if (line.length()>50&&line.contains("logid")&&line.contains("requestip")) {
-                BaseMr baseMr = new BaseMr(line);
-                Map subMap = baseMr.getSubMap();
-                String day_id = subMap.get("day_id").toString();
-                String hour_id = subMap.get("hour_id").toString();
-                String requestIp = baseMr.sub("requestip");
-                String cityCode = subMap.get("cityCode").toString();
-                String cityName = baseMr.sub("city_name");
-                String regionCode = subMap.get("regionCode").toString();
-                String latncode = subMap.get("latnCode").toString();
-                String requesttype = baseMr.sub("requesttype");
-                String channelno = baseMr.sub("channelno");
-                String responsecode = baseMr.sub("responsecode");
-                String imei = subMap.get("imei").toString();
-                String code = baseMr.sub("code");
-                String typecode1 = baseMr.sub("typecode1");
-                String typecode2 = baseMr.sub("typecode2");
-                // 组装
-                String keyValue = day_id + "\t" + hour_id + "\t" + requestIp + "\t"
-                        + cityCode + "\t" + cityName + "\t"
-                        + regionCode + "\t" + latncode + "\t" + requesttype + "\t" + channelno
-                        + "\t" + responsecode
-                        + "\t" + imei + "\t" + code + "\t" + typecode1 + "\t" + typecode2;
+            if (line.length() <= 50 || !line.contains("logid") || !line.contains("requestip"))
+                return;
+            BaseMr baseMr = new BaseMr(line);
+            Map subMap = baseMr.getSubMap();
+            String day_id = subMap.get("day_id").toString();
+            String hour_id = subMap.get("hour_id").toString();
+            String requestIp = baseMr.sub("requestip");
+            String cityCode = subMap.get("cityCode").toString();
+            String cityName = baseMr.sub("city_name");
+            String regionCode = subMap.get("regionCode").toString();
+            String latncode = subMap.get("latnCode").toString();
+            String requesttype = baseMr.sub("requesttype");
+            String channelno = baseMr.sub("channelno");
+            String responsecode = baseMr.sub("responsecode");
+            String imei = subMap.get("imei").toString();
+            String code = baseMr.sub("code");
+            String typecode1 = baseMr.sub("typecode1");
+            String typecode2 = baseMr.sub("typecode2");
+            // 组装
+            String keyValue = day_id + "\t" + hour_id + "\t" + requestIp + "\t"
+                    + cityCode + "\t" + cityName + "\t"
+                    + regionCode + "\t" + latncode + "\t" + requesttype + "\t" + channelno
+                    + "\t" + responsecode
+                    + "\t" + imei + "\t" + code + "\t" + typecode1 + "\t" + typecode2;
 
-                word.set(keyValue);
-                context.write(word, one);
-            }
+            word.set(keyValue);
+            context.write(word, one);
         }
     }
     public static class OnOffLog4HiveReducer extends
